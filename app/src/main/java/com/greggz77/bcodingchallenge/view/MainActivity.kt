22516eea14d31be13ba163050084c1d5bb3af832
gregz <gregor@zafran.si>
2019-02-04 23:43:00 +0100

    added toolbar

--- app/src/main/java/com/greggz77/bcodingchallenge/view/MainActivity.kt
+++ app/src/main/java/com/greggz77/bcodingchallenge/view/MainActivity.kt
@@ -2,12 +2,13 @@ package com.greggz77.bcodingchallenge.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.greggz77.bcodingchallenge.R
-import com.greggz77.bcodingchallenge.contract.ContractInterface
+import com.greggz77.bcodingchallenge.contract.ContractInterface.View
 import com.greggz77.bcodingchallenge.presenter.MainActivityPresenter
 
 
-class MainActivity : AppCompatActivity(), ContractInterface.View {
+class MainActivity : AppCompatActivity(), View {
 
     private var presenter: MainActivityPresenter? = null
 
@@ -15,6 +16,14 @@ class MainActivity : AppCompatActivity(), ContractInterface.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //implements action bar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        //displays in-app position
+
+        //displays back button
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         presenter = MainActivityPresenter(this)
 
